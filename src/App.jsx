c3df7f1cd73f49412431f02d3c6f5949038c559f
Fatma <fatma.arslantas@outlook.com>
2026-05-19 18:58:08 +0100

    Add Player component and initial application styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
+import Player from "./components/Player";
+import "./App.css";
+
 function App() {
   return (
     <main>
       <h1>Memory Match</h1>
       <div id="game-container">
-        {/* Game components will go here */}
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName='Player 1'
+            score={0}
+            isActive={true}
+          />
+          <Player
+            initialName='Player 2'
+            score={0}
+            isActive={false}
+          />         
+        </ol>
       </div>
     </main>
   );
---
+++ src/components/Player.jsx
@@ -0,0 +1,40 @@
+import { use, useState } from "react";
+
+export default function Player({ initialName, score, isActive }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input 
+        type="text"
+        required
+        value={playerName}
+        onChange={handleChange} 
+      />
+    );
+  }
+
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-score"> Matches: {score} </span>
+      </span>
+      <button onClick={handleEditClick}>
+        {isEditing ? "Save" : "Edit"}
+
+      </button>
+    </li>
+  );
+}
